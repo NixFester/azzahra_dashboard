91b3f31eb5f72f348f3f53fafaf232c31bfcfa26
NixFester <abi00kun@gmail.com>
2026-02-12 10:55:33 +0700

    dor

--- application/models/test_accounts_model.php
+++ application/models/test_accounts_model.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class test_accounts_model extends CI_Model {
 
-    private $table = 'your_table_name';
+    private $table = 'karyawan';
 
     public function insert($data) {
         return $this->db->insert($this->table, $data);
